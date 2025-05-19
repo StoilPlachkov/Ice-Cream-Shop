07a6425da419039d21f6a3054b1d608026dbc413
Stoil Plachkov <134845487+StoilPlachkov@users.noreply.github.com>
2025-05-19 18:15:48 +0300

    Delete ShopFactory.java

--- ShopFactory.java
+++
@@ -1,9 +0,0 @@
-public abstract class ShopFactory {
-    public abstract IceCream createIceCream(String flavour);
-
-    public IceCream prepareIceCream(String flavour){
-        IceCream iceCream = this.createIceCream(flavour);
-        iceCream.prepare();
-        return iceCream;
-    }
-}
